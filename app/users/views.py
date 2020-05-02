7fde2151eecb5627d02abfac29ad8dc67b6344df
Vitaly <dubovetsvitaliy@gmail.com>
2020-05-02 23:55:46 +0500

    Finished to create all functions of users api

--- app/users/views.py
+++ app/users/views.py
@@ -31,7 +31,7 @@ class UserCollectionAPI(Resource):
             db.session.commit()
         except IntegrityError:
             abort(400, message='User already exists')
-        return {'message': f'User "{user.username}" created successfully',
+        return {'message': f'User created successfully.',
                 'user': serializer.dump(user)}, 201
 
 
@@ -44,10 +44,39 @@ class UserAPI(Resource):
         super(UserAPI, self).__init__()
 
     def get(self, user_id):
-        return {'msg': user_id}
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            abort(400, message="User doesn't exist")
+        serializer = UserSerializer()
+        return serializer.dump(user)
 
-    def put(self, user_id):
-        pass
+    def patch(self, user_id):
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            abort(400, message="User doesn't exist")
+        args = self.reqparse.parse_args()
+        updated_data = args.copy()
+        for k, v in args.items():
+            if not v:
+                updated_data.pop(k)
+        if not updated_data:
+            abort(400, message='Arguments is empty in json')
+        serializer = UserSerializer()
+        for k, v in updated_data.items():
+            setattr(User, k, v)
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except IntegrityError:
+            abort(400, message='A user with this name or email already exists')
+        return {'updated_data': updated_data, 'user': serializer.dump(user)}, 200
 
     def delete(self, user_id):
-        pass
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            abort(400, message="User is already deleted")
+        serializer = UserSerializer()
+        user_data = serializer.dump(user)
+        db.session.delete(user)
+        db.session.commit()
+        return {'message': f'User deleted successfully.', 'user': user_data}, 204
